957016b2897af053a8fb3e6b93a2a225d1fcda78
Basil <mbonubasil@gmail.com>
2018-09-03 08:57:27 +0100

    Complete frontend design mockups

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 // 1. Create a student or register
-// 2. update student 
-// 3. deolete student
-// 4. display student info 
-// 5. Register course 
-// 6. withdraw course 
-// 7. Logout (exit) 
+// 2. update student
+// 3. delete student
+// 4. display student info
+// 5. Register course
+// 6. withdraw course
+// 7. Logout (exit)
 
 // store student cousese in an Array
 // Each course will have (id, name, hours)
@@ -18,7 +17,5 @@ const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("Welcome to student portal");
 });
-
-app.()
 
 app.listen(port, () => console.log(`app started on port ${port}`));
